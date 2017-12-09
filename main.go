4ff5c481a90b32bdb2f2783a24c4b2015268e625
MarilynWaldman <mdwaldman22@hotmail.com>
2017-12-08 21:14:42 -0700

    added closure

--- main.go
+++ main.go
@@ -3,10 +3,24 @@ package main
 import (
 	"fmt"
 	src "./src"
+	"time"
+
 )
+func greet(name string) {
+	msg := name + fmt.Sprintf(" you arrived at (at %v)", time.Now().String())
+
+	closure := func() {
+		fmt.Printf("Hey %s!n", msg)
+	}
+	closure()
+
+}
 
 func main() {
 	fib := src.MemorizedFib;
 	fmt.Printf("Fib: %w\n", fib(8))
+	greet("Alice")
+
+
 }
 
